72de588a8f8c1f70fbdf2c5152b945a7ee180e41
Lin_WK <linting98520@gmail.com>
2026-04-10 17:13:06 +0800

    260410_working

---
+++ Assets/Danmaku/Scripts/Shooter/LinearProjectile.cs
@@ -0,0 +1,37 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Burst;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
+
+public struct LinearMoveData : IComponentData
+{
+    public float Speed;
+    public float3 Direction;
+}
+
+
+[BurstCompile]
+public partial struct LinearMoveSystem : ISystem
+{
+    public void OnUpdate(ref SystemState state) 
+    {
+        state.Dependency = new LinearMoveJob
+        {
+            DeltaTime = SystemAPI.Time.DeltaTime
+        }.ScheduleParallel(state.Dependency);
+    }
+}
+
+[BurstCompile]
+public partial struct LinearMoveJob : IJobEntity
+{
+    public float DeltaTime;
+    public void Execute(LocalTransform localTransform, in LinearMoveData data)
+    {
+        localTransform.Position += data.Direction * data.Speed * DeltaTime;
+    }
+}
---
+++ Assets/Danmaku/Scripts/Shooter/RadialShooterSystem.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Burst;
+using Unity.Entities;
+using UnityEngine;
+
+[BurstCompile]
+public partial struct RadialShooterSpawnSystem : ISystem
+{
+    [BurstCompile]
+    public void OnUpdate(ref SystemState state) 
+    {
+        
+    }
+}
